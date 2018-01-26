4fc03d23204bc0bd099728100971f502f45a1a9f
17nouri <7nouri@informatik.uni-hamburg.de>
2018-01-26 09:18:48 +0100

    one less layer in disc and gen

--- workingPokemonGAN.py
+++ workingPokemonGAN.py
@@ -12,7 +12,7 @@ image_dim = HEIGHT * WIDTH * CHANNEL
 noise_dim = 100
 h_dim = 128
 
-data_directory = "./ourDataset/all"
+data_directory = "./ourDataset/filter"
 
 # Reading in the pictures
 def process_data():
@@ -85,8 +85,8 @@ def lrelu(x, n, leak=0.2):
 
 #Source : https://github.com/llSourcell/Pokemon_GAN/blob/master/pokeGAN.py
 def generator(input, random_dim,  reuse=False):
-    c4, c8, c16, c32, c64 = 512, 256, 128, 64, 32  # channel num
-    s4 = 4
+    c4, c8, c16 = 512, 256, 128  # channel num
+    s4 = 8
     output_dim = CHANNEL  # RGB image
     with tf.variable_scope('gen') as scope:
         if reuse:
@@ -96,12 +96,13 @@ def generator(input, random_dim,  reuse=False):
         b1 = tf.get_variable('b1', shape=[c4 * s4 * s4], dtype=tf.float32,
                              initializer=tf.constant_initializer(0.0))
         flat_conv1 = tf.add(tf.matmul(input, w1), b1, name='flat_conv1')
+        # 8*8*512
         # Convolution, bias, activation, repeat!
         conv1 = tf.reshape(flat_conv1, shape=[-1, s4, s4, c4], name='conv1')
         bn1 = tf.contrib.layers.batch_norm(conv1, is_training=True, epsilon=1e-5, decay=0.9,
                                            updates_collections=None, scope='bn1')
         act1 = tf.nn.relu(bn1, name='act1')
-        # 8*8*256
+        # 16*16*256
         # Convolution, bias, activation, repeat!
         conv2 = tf.layers.conv2d_transpose(act1, c8, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
@@ -109,33 +110,27 @@ def generator(input, random_dim,  reuse=False):
         bn2 = tf.contrib.layers.batch_norm(conv2, is_training=True, epsilon=1e-5, decay=0.9,
                                            updates_collections=None, scope='bn2')
         act2 = tf.nn.relu(bn2, name='act2')
-        # 16*16*128
+        # 32*32*128
         conv3 = tf.layers.conv2d_transpose(act2, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
                                            name='conv3')
         bn3 = tf.contrib.layers.batch_norm(conv3, is_training=True, epsilon=1e-5, decay=0.9,
                                            updates_collections=None, scope='bn3')
         act3 = tf.nn.relu(bn3, name='act3')
-        # 32*32*64
-        conv4 = tf.layers.conv2d_transpose(act3, c32, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+
+        # 64*64*3
+        conv4 = tf.layers.conv2d_transpose(act3, output_dim, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
                                            name='conv4')
-        bn4 = tf.contrib.layers.batch_norm(conv4, is_training=True, epsilon=1e-5, decay=0.9,
-                                           updates_collections=None, scope='bn4')
-        act4 = tf.nn.relu(bn4, name='act4')
-        # 64*64*3
-        conv5 = tf.layers.conv2d_transpose(act4, output_dim, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv5')
         #bn5 = tf.contrib.layers.batch_norm(conv5, is_training=True, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn5')
-        act5 = tf.nn.sigmoid(conv5, name='act5')
-        return act5
+        act4 = tf.nn.sigmoid(conv4, name='act4')
+        return act4
 
 
 
 #Source : https://github.com/llSourcell/Pokemon_GAN/blob/master/pokeGAN.py
 def discriminator(input, reuse=False):
-    c2, c4, c8, c16 = 64, 128, 256, 512  # channel num: 64, 128, 256, 512
+    c2, c4, c8 = 128, 256, 512  # channel num: 64, 128, 256, 512
     with tf.variable_scope('dis') as scope:
         if reuse:
             scope.reuse_variables()
@@ -163,17 +158,11 @@ def discriminator(input, reuse=False):
         bn3 = tf.contrib.layers.batch_norm(conv3, is_training=True, epsilon=1e-5, decay=0.9,
                                            updates_collections=None, scope='bn3')
         act3 = lrelu(bn3, n='act3')
-        # Convolution, activation, bias, repeat!
-        conv4 = tf.layers.conv2d(act3, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                 name='conv4')
-        bn4 = tf.contrib.layers.batch_norm(conv4, is_training=True, epsilon=1e-5, decay=0.9,
-                                           updates_collections=None, scope='bn4')
-        act4 = lrelu(bn4, n='act4')
+
 
         # start from act4
-        dim = int(np.prod(act4.get_shape()[1:]))
-        fc1 = tf.reshape(act4, shape=[-1, dim], name='fc1')
+        dim = int(np.prod(act3.get_shape()[1:]))
+        fc1 = tf.reshape(act3, shape=[-1, dim], name='fc1')
 
         w2 = tf.get_variable('w2', shape=[fc1.shape[-1], 1], dtype=tf.float32,
                              initializer=tf.truncated_normal_initializer(stddev=0.02))

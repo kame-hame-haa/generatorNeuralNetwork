bf72b96afbdd1fd75082e3d3fbaf761d4ef17e6d
Kim-Morgaine Lohse <7lohse@informatik.uni-hamburg.de>
2018-02-01 15:59:45 +0100

    Schönerer Code mit mehr Kommentaren

--- gan_better.py
+++ gan_better.py
@@ -12,8 +12,8 @@ h_dim = 128
 dropoutRate = 0.7
 alplr = 0.2 #leaky Relu
 
-INPUT_CHANNEL = 1 #1 bei grauer Farbe
-NUMBER_1CNN = 64  # Die Outputs des ersten convolution layer
+INPUT_CHANNEL = 1 #1 MNIST
+NUMBER_1CNN = 64  # 
 
 mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
@@ -39,8 +39,7 @@ def plot(samples):
     return fig
 
 
-# normalisiert erstellte Matrizen; besser als 0 - Matrizen
-#vermeidet das die Matrix mit null initialisiert wird und macht eine Normalverteilung
+# normal distribution for iniatilisation
 def xavier_init(size):
     in_dim = size[0]
     xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
@@ -50,61 +49,58 @@ def xavier_init(size):
 def sample_z(m, n):
     return np.random.uniform(-1., 1., size=[m, n])
 
-#x ist der Einstiegspunkt, Vektor
-#W1 und W2 sind Matrizen
-#b1 und b2 sind Bias
-#Ausgegeben wird die Matrixmultiplikation
+# x is image input
 def discriminator(x, D_W1, D_W2, D_b1, D_b2):
-    # D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
-    x_shaped = tf.reshape(x, [-1, 28, 28, INPUT_CHANNEL])  #Erstellt eine Matrix 28*28, -1: Wenn mehr Werte kommen, soll es alle am Anfang stehen
-    conv1 = create_new_conv_layer(x_shaped, INPUT_CHANNEL, NUMBER_1CNN, [5, 5], [2, 2],
-                                  name='cnnlayer1')  # Stride [2, 2] sorgt für Halbierung des Outputs 28 -> 14
+    with tf.name_scope('discriminator'):
+        x_shaped = tf.reshape(x, [-1, 28, 28, INPUT_CHANNEL])
+        conv1 = create_new_conv_layer(x_shaped, INPUT_CHANNEL, NUMBER_1CNN, [5, 5], [2, 2],
+                                      name='cnnlayer1')  # Stride [2, 2] halfs input
 
-    flattened = tf.reshape(conv1, [-1, 14 * 14 * NUMBER_1CNN])
+        flattened = tf.reshape(conv1, [-1, 14 * 14 * NUMBER_1CNN])
 
-    # setup some weights and bias values for this layer, then activate with lrelu
-    # fasst Output von CNN zusammen
-    dense_layer1 = tf.matmul(flattened, D_W1) + D_b1
-    dense_layer1 = lrelu(dense_layer1, alplr)
+       
+        # dense for output
+        dense_layer1 = tf.matmul(flattened, D_W1) + D_b1
+        dense_layer1 = lrelu(dense_layer1, alplr)
 
-    out = lrelu(dense_layer1, alplr)
+        out = lrelu(dense_layer1, alplr)
 
-    return out
+        return out
 
-#num_input = 1 wenn es grau sein soll, 3 bei Farben
 def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_shape, stride, name):
-    # setup the filter input shape for tf.nn.conv_2d
-    conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels,
-                       num_filters]
+    with tf.name_scope('convolution'):
+        # setup the filter input shape for tf.nn.conv_2d
+        conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels,
+                           num_filters]
 
-    # initialise weights and bias for the filter
-    weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03),
-                          name=name + '_W')
-    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name + '_b')
+        # initialise weights and bias for the filter
+        weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03),
+                              name=name + '_W')
+        bias = tf.Variable(tf.truncated_normal([num_filters]), name=name + '_b')
 
-    # setup the convolutional layer operation
-    conv1 = tf.nn.conv2d(input_data, weights, [1, stride[0], stride[1], 1], padding='SAME')
+        # setup the convolutional layer operation
+        conv1 = tf.nn.conv2d(input_data, weights, [1, stride[0], stride[1], 1], padding='SAME')
 
-    # add the bias
-    conv1 += bias
+        # add the bias
+        conv1 += bias
 
-    # apply a ReLU non-linear activation
+        # apply a ReLU non-linear activation
 
-    conv1 = lrelu(conv1, alplr)
+        conv1 = lrelu(conv1, alplr)
 
-    return conv1
+        return conv1
 
 
-with tf.name_scope('model1'):
+with tf.name_scope('model'):
     # generator variabeln
 
     z = tf.placeholder(tf.float32, shape=[None, z_dim])
 
-    G_W1 = tf.Variable(xavier_init([z_dim, h_dim]))
-    G_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+    G_W1 = tf.Variable(xavier_init([z_dim, h_dim]), name='G_W1')
+    G_b1 = tf.Variable(tf.zeros(shape=[h_dim]), name='G_b1')
 
-    G_W2 = tf.Variable(xavier_init([h_dim, X_dim]))
-    G_b2 = tf.Variable(tf.zeros(shape=[X_dim]))
+    G_W2 = tf.Variable(xavier_init([h_dim, X_dim]), name='G_W2')
+    G_b2 = tf.Variable(tf.zeros(shape=[X_dim]), name='G_b2')
 
     theta_G = [G_W1, G_W2, G_b1, G_b2]
 
@@ -112,28 +108,24 @@ with tf.name_scope('model1'):
 
     X = tf.placeholder(tf.float32, shape=[None, X_dim])
 
-    # wd1 = tf.Variable(tf.truncated_normal([14 * 14 * 32, 1000], stddev=0.03), name='wd1')
-    # bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
-    D_W1 = tf.Variable(xavier_init([14 * 14 * NUMBER_1CNN, 1000]))
-    D_b1 = tf.Variable(tf.zeros(shape=[1000]))
+    D_W1 = tf.Variable(xavier_init([14 * 14 * NUMBER_1CNN, 1000]), name='D_W1')
+    D_b1 = tf.Variable(tf.zeros(shape=[1000]), name='D_b1')
 
-    # wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
-    # bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
 
-    D_W2 = tf.Variable(xavier_init([1000, 1]))
-    D_b2 = tf.Variable(tf.zeros(shape=[1]))
+    D_W2 = tf.Variable(xavier_init([1000, 1]), name='D_W2')
+    D_b2 = tf.Variable(tf.zeros(shape=[1]), name='D_b2')
 
     theta_D = [D_W1, D_W2, D_b1, D_b2]
+    with tf.name_scope('Generator'):
+        # generator
+        keepProb = tf.placeholder(tf.float32)
+        G_h1 = lrelu(tf.matmul(z, G_W1) + G_b1, alplr)
+        G_h1Drop = tf.nn.dropout(G_h1, keepProb)  # dropout just if generators is trained
+        G_log_prob = tf.matmul(G_h1Drop, G_W2) + G_b2
 
-    # generator
-    keepProb = tf.placeholder(tf.float32)
-    G_h1 = lrelu(tf.matmul(z, G_W1) + G_b1, alplr)
-    G_h1Drop = tf.nn.dropout(G_h1, keepProb)  # drop beim Testen und nihct
-    G_log_prob = tf.matmul(G_h1Drop, G_W2) + G_b2
+        # dropout Layer
 
-    # dropout Layer
-
-    G_sample = tf.nn.sigmoid(G_log_prob)
+        G_sample = tf.nn.sigmoid(G_log_prob)
 
     # discriminator
 
@@ -143,13 +135,14 @@ with tf.name_scope('model1'):
 with tf.name_scope('train'):
     D_loss = tf.reduce_mean(D_real) - tf.reduce_mean(D_fake)
     G_loss = -tf.reduce_mean(D_fake)
+    tf.summary.scalar('D_loss', D_loss) # documentation for tensorboard 
+    tf.summary.scalar('G_loss', G_loss)
 
     D_solver = (tf.train.RMSPropOptimizer(learning_rate=1e-4)
                 .minimize(-D_loss, var_list=theta_D))
     G_solver = (tf.train.RMSPropOptimizer(learning_rate=1e-4)
                 .minimize(G_loss, var_list=theta_G))
 
-    clip_D = [p.assign(tf.clip_by_value(p, -0.01, 0.01)) for p in theta_D]
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -159,28 +152,35 @@ if not os.path.exists('out/'):
 
 i = 0
 
+tfb_merged = tf.summary.merge_all()# documentation for tensorboard 
+train_writer = tf.summary.FileWriter('log/', sess.graph)
+
+
 for it in range(1000000):
     for _ in range(5):
         X_mb, _ = mnist.train.next_batch(mb_size)
 
-        _, D_loss_curr, _ = sess.run(
-            [D_solver, D_loss, clip_D],
-            feed_dict={X: X_mb, z: sample_z(mb_size, z_dim), keepProb: 1.0} #Durchlauf, wo wir den Discriminator trainieren
+        _, D_loss_curr, summary = sess.run(
+            [D_solver, D_loss, tfb_merged],
+            feed_dict={X: X_mb, z: sample_z(mb_size, z_dim), keepProb: 1.0} #training discriminator
         )
+        train_writer.add_summary(summary, it)# documentation for tensorboard 
 
     _, G_loss_curr = sess.run(
         [G_solver, G_loss],
-        feed_dict={z: sample_z(mb_size, z_dim), keepProb: 1.0} #Durchlauf, wo wir den Generator trainieren
+        feed_dict={z: sample_z(mb_size, z_dim), keepProb: 0.7} #training generator so dropout is used
     )
+    train_writer.add_summary(summary, it)# documentation for tensorboard 
+
 
     if it % 100 == 0:
         print('Iter: {}; D loss: {:.4}; G_loss: {:.4}'
               .format(it, D_loss_curr, G_loss_curr))
+        tf.summary.merge_all()
 
         if it % 500 == 0:
             samples = sess.run(G_sample, feed_dict={z: sample_z(16, z_dim), keepProb: 1.0})
-            # print(samples)
-
+            
             fig = plot(samples)
             plt.savefig('out/{}.png'
                         .format(str(i).zfill(3)), bbox_inches='tight')
